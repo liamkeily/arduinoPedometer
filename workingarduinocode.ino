fbf77e0cdb6db22a14ceb9e099e0fc6b4f7bf934
Liam <liam@simplywebsites.com>
2014-03-25 12:23:33 +0000

    Latest code

--- workingarduinocode.ino
+++ workingarduinocode.ino
@@ -1,39 +1,34 @@
 /*
-  LiquidCrystal Library - Hello World
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
- 
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
+LiquidCrystal Library - Hello World
+Demonstrates the use a 16x2 LCD display. The LiquidCrystal
+library works with all LCD displays that are compatible with the
+Hitachi HD44780 driver. There are many of them out there, and you
+can usually tell them by the 16-pin interface.
+This sketch prints "Hello World!" to the LCD
+and shows the time.
+The circuit:
+* LCD RS pin to digital pin 12
+* LCD Enable pin to digital pin 11
+* LCD D4 pin to digital pin 5
+* LCD D5 pin to digital pin 4
+* LCD D6 pin to digital pin 3
+* LCD D7 pin to digital pin 2
+* LCD R/W pin to ground
+* 10K resistor:
+* ends to +5V and ground
+* wiper to LCD VO pin (pin 3)
+Library originally added 18 Apr 2008
+by David A. Mellis
+library modified 5 Jul 2009
+by Limor Fried (http://www.ladyada.net)
+example added 9 Jul 2009
+by Tom Igoe
+modified 22 Nov 2010
+by Tom Igoe
+This example code is in the public domain.
 
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
+http://www.arduino.cc/en/Tutorial/LiquidCrystal
+*/
 
 // include the library code:
 #include <LiquidCrystal.h>
@@ -48,11 +43,21 @@ int t = 0;
 
 int accread = 0;
 int tempread = 0;
+float lastx = 0;
+float lasty = 0;
+float lastz = 0;
+float acceleration;
+int steps;
+float distance = 0;
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   Serial.begin(9600);
+  
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
@@ -61,18 +66,47 @@ void loop() {
   // print the number of seconds since reset:
     int time = millis();
     if(accread < time-100){
-    x = analogRead(A0);
-    y = analogRead(A1);
-    z = analogRead(A2);
-    accread = time;
-    }
-    if(tempread < time-1000){
-    t = map(analogRead(A3),0,1023,0,125);
-    tempread = time;
+      x = analogRead(A0);
+      y = analogRead(A1);
+      z = analogRead(A2);
+      acceleration = sqrt((x-lastx)*(x-lastx) + (y-lasty)*(y-lasty) + (z-lastz)*(z-lastz));
+      
+      if(acceleration > 30){
+          steps++;
+          Serial.print("Step!");
+          delay(300);
+          
+          digitalWrite(10,LOW);
+          digitalWrite(9,LOW);
+          digitalWrite(8,LOW);
+          
+          if(steps>10){
+          digitalWrite(10,HIGH);
+          }
+         if(steps>20){
+          digitalWrite(9,HIGH);
+         }
+         if(steps>30){
+          digitalWrite(8,HIGH);
+         }
+
+      }
+      lastx = x;
+      lasty = y;
+      lastz = z;
+      
+      accread = time;
+      
+      if(tempread < time-1000){
+      tempread = time;
+      t = map(analogRead(A3),0,1023,0,125);
+      }
     }
   
   lcd.setCursor(0, 0);
-  lcd.print("X: "+ String(x) + " Y: " + String(y));
+  lcd.print("Temperature: " + String(t));
+  
   lcd.setCursor(0, 1);
-  lcd.print(" Z: "+ String(z) + " T: " + t);
+  lcd.print("Steps: " + String(steps));
+ 
 }
